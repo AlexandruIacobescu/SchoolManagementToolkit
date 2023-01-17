ecfcbe90b316759254c5ca0954c4b1035d55fc50
ALEX <alexandru.iacobescu01@e-uvt.ro>
2023-01-18 01:40:07 +0200

    commit_2

--- src/main/java/com/wut/school_management_toolkit/controller/StudentController.java
+++ src/main/java/com/wut/school_management_toolkit/controller/StudentController.java
@@ -2,41 +2,63 @@ package com.wut.school_management_toolkit.controller;
 
 import com.wut.school_management_toolkit.entity.Student;
 import com.wut.school_management_toolkit.repository.StudentRepository;
+import com.wut.school_management_toolkit.service.StudentService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/students")
 public class StudentController {
-    @Autowired
-    private StudentRepository studentRepository;
 
-    @RequestMapping("/get-all")
-    public List<Student> findAllStudents(){
-        return studentRepository.findAll();
+    private final StudentService studentService;
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentController(StudentService studentService,
+                             StudentRepository studentRepository) {
+        this.studentService = studentService;
+        this.studentRepository = studentRepository;
+    }
+
+    @GetMapping("/get-all")
+    public ResponseEntity<List<Student>> findAllStudents(){
+        List<Student> students = studentService.findAllStudents();
+        return new ResponseEntity<>(students, HttpStatus.OK);
     }
 
     @PostMapping("/insert")
-    public Student createStudent(@RequestBody final Student student){
-        return studentRepository.saveAndFlush(student);
+    public ResponseEntity<Student> createStudent(@RequestBody final Student student){
+        Student newStudent = studentService.addStudent(student);
+        return new ResponseEntity<>(newStudent, HttpStatus.OK);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Student> updateStudent(@RequestBody Student student){
+        Student updateStudent = studentService.updateStudent(student);
+        return new ResponseEntity<>(updateStudent, HttpStatus.OK);
     }
 
     @DeleteMapping("/delete-by-id/{id}")
-    public void deleteStudentById(@PathVariable("id") final Long id){
-        studentRepository.deleteStudentByStudentId(id);
+    public ResponseEntity<?> deleteStudentById(@PathVariable("id") final Long id){
+        studentService.deleteStudentById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @RequestMapping("/find-by-firstname-and-lastname/{fname}/{lname}")
-    public List<Student> findStudentsByFirstNameAndLastName(@PathVariable("fname") final String fname,
-                                                            @PathVariable("lname") final String lname){
-        return studentRepository.findStudentsByFirstNameAndLastName(fname, lname);
+    public ResponseEntity<List<Student>> findStudentsByFirstNameAndLastName(@PathVariable("fname") final String fname,
+                                                                            @PathVariable("lname") final String lname){
+        List<Student> students = studentService.findStudentsByFirstNameAndLastName(fname, lname);
+        return new ResponseEntity<>(students, HttpStatus.OK);
     }
 
     @RequestMapping("/find-if-year-of-study-greater-than/{yos}")
-    public List<Student> findStudentsWithYearOfStudyGreaterThan1(@PathVariable("yos") final Integer yos){
-        return studentRepository.findStudentsByYearOfStudyGreaterThan(yos);
+    public ResponseEntity<List<Student>> findStudentsWithYearOfStudyGreaterThan1(@PathVariable("yos") final Integer yos){
+        List<Student> students = studentRepository.findStudentsByYearOfStudyGreaterThan(yos);
+        return new ResponseEntity<>(students, HttpStatus.OK);
     }
 }
---
+++ src/main/java/com/wut/school_management_toolkit/service/StudentService.java
@@ -0,0 +1,42 @@
+package com.wut.school_management_toolkit.service;
+
+import com.wut.school_management_toolkit.entity.Student;
+import com.wut.school_management_toolkit.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public Student addStudent(Student student){
+        return studentRepository.saveAndFlush(student);
+    }
+
+    public List<Student> findAllStudents(){
+        return studentRepository.findAll();
+    }
+
+    public Student updateStudent(Student student){
+        return studentRepository.save(student);
+    }
+
+    public void deleteStudentById(Long id){
+        studentRepository.deleteStudentByStudentId(id);
+    }
+
+    public List<Student> findStudentsByFirstNameAndLastName(String fname, String lname){
+        return studentRepository.findStudentsByFirstNameAndLastName(fname, lname);
+    }
+
+    public List<Student> findStudentsWithYearOfStudyGreaterThan1(Integer yos){
+        return studentRepository.findStudentsByYearOfStudyGreaterThan(yos);
+    }
+}
